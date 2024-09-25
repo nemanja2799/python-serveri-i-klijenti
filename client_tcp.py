5e09a0c88afc28c84074c701f6922054f333c244
nemanja2799 <nemanjaivanovic2799@gmail.com>
2024-09-25 15:14:53 +0200

    Primer tcp i udp servera< i klijenata za oba servisa.

---
+++ client_tcp.py
@@ -0,0 +1,23 @@
+import socket
+import time
+import struct
+
+
+def start_client(host='192.168.68.181', port=65433):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        print(f"Connected to {host}:{port}")
+
+        while True:
+            # Example array of integers to send
+            int_array = [1, 2, 3, 4, 5]
+            # Convert the array of integers to bytes
+            data = struct.pack(f'{len(int_array)}i', *int_array)
+            # Send the byte data
+            s.sendall(data)
+            print(f"Sent data: {int_array}")
+            time.sleep(1)
+
+
+#if _name_ == "__main__":
+start_client()
